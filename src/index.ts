5b6cdd6a43c8b3de42279f8242c3f8d330985b92
adambelton <thelastgreatcynic@gmail.com>
2018-06-08 10:11:44 +0200

    add authentication

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,23 @@
 import 'reflect-metadata'
-import {createKoaServer} from "routing-controllers"
+import { createKoaServer, Action } from 'routing-controllers'
 import setupDb from './db'
-import QuizController from './quizzes/controller';
-import QuestionsController from './questions/controller';
+import QuizController from './quizzes/controller'
+import QuestionsController from './questions/controller'
 
 const port = process.env.PORT || 4001
 
 const app = createKoaServer({
   cors: true,
-  controllers: [
-    QuizController,
-    QuestionsController
-  ]
+  controllers: [QuizController, QuestionsController],
+  currentUserChecker: async (action: Action) => {
+    const userId: number = action.request.headers['x-user-id']
+    const isTeacher: boolean = action.request.headers['x-user-isteacher']
+
+    console.log('userId', userId)
+    console.log('isTeacher', isTeacher)
+
+    return { userId, isTeacher }
+  }
 })
 
 setupDb()
--- src/quizzes/controller.ts
+++ src/quizzes/controller.ts
@@ -1,39 +1,49 @@
-import { JsonController, NotFoundError, Post, HttpCode, Get, Body, Param, Delete } from 'routing-controllers'
+import {
+  JsonController,
+  NotFoundError,
+  Post,
+  HttpCode,
+  Get,
+  Body,
+  Param,
+  Delete,
+  CurrentUser
+} from 'routing-controllers'
 import Quiz from './entity'
+
+type User = {
+  userId: number
+  isTeacher: boolean
+}
 
 @JsonController()
 export default class QuizController {
+  @Get('/quizzes/:id')
+  getQuiz(@Param('id') id: number) {
+    return Quiz.findOneById(id)
+  }
 
-    @Get('/quizzes/:id')
-    getQuiz(
-        @Param('id') id:  number
-    ) {
-        return Quiz.findOneById(id)
-    }
+  @Get('/quizzes')
+  async allQuizzes(@CurrentUser() user: User) {
+    console.log(user)
 
-    @Get('/quizzes')
-    async allQuizzes(){
-        const quizzes = await Quiz.find()
-        return { quizzes }
-    }
+    const quizzes = await Quiz.find()
+    return { quizzes }
+  }
 
-    @Post('/quizzes')
-    @HttpCode(201)
-    createQuiz(
-        @Body() quiz: Quiz
-    ) {
-        return quiz.save()
-    }
+  @Post('/quizzes')
+  @HttpCode(201)
+  createQuiz(@Body() quiz: Quiz) {
+    return quiz.save()
+  }
 
-    @Delete('/quizzes/:id')
-    async deleteQuiz(
-        @Param('id') id: number 
-    ) {
-        const quiz = await Quiz.findOneById(id)
+  @Delete('/quizzes/:id')
+  async deleteQuiz(@Param('id') id: number) {
+    const quiz = await Quiz.findOneById(id)
 
-        if (!quiz) throw new NotFoundError('Nothing to Delete here!')
+    if (!quiz) throw new NotFoundError('Nothing to Delete here!')
 
-        if (quiz) Quiz.removeById(id)
-        return 'Quiz Deleted.'
-    }
+    if (quiz) Quiz.removeById(id)
+    return 'Quiz Deleted.'
+  }
 }
